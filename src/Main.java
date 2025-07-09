b0d667498edb20cd42810b4dd14bfaf481207723
Backspac3_N00B <173277005+ImOutOfBounds@users.noreply.github.com>
2025-07-09 12:35:30 -0300

    função tokenize
    primeira versão da função de tokenização, iterando cada char do arquivo, adicionanado a tag <whit> para os casos de espaço em branco e printando o resultado

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,36 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        String[] text = new String[]{Files.readString(Paths.get("./LScode/teste.ls"))};
+        String content = Files.readString(Paths.get("./LScode/teste2.ls"));
 
-        for(String word : text){
-            //System.out.printf(word);
+        ArrayList<String> tokens = tokenize(content);
+
+        printTokens(tokens);
+    }
+
+    public static ArrayList<String> tokenize(String content){
+        ArrayList<String> tokens = new ArrayList<>();
+
+        for (int i = 0; i < content.length(); i++) {
+            char ch = content.charAt(i);
+
+            if (ch == ' ') {
+                tokens.add("<whit>");
+            }
+
+        }
+
+        return tokens;
+    }
+
+    public static void printTokens(ArrayList<String> tokensList){
+        for(String tok : tokensList){
+            System.out.println(tok);
         }
     }
 }
