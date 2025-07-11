8d05af10767a8601320dfebe1e65974feebd8cb0
Backspac3_N00B <173277005+ImOutOfBounds@users.noreply.github.com>
2025-07-11 16:50:34 -0300

    primeira versao tokenizer funcional

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import dto.TokenDTO;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,19 +10,48 @@ public class Main {
 
         String content = Files.readString(Paths.get("./LScode/teste2.ls"));
 
-        ArrayList<String> tokens = tokenize(content);
+        ArrayList<TokenDTO> tokens = tokenize(content);
 
         printTokens(tokens);
     }
 
-    public static ArrayList<String> tokenize(String content){
-        ArrayList<String> tokens = new ArrayList<>();
+    public static ArrayList<TokenDTO> tokenize(String content){
+        ArrayList<TokenDTO> tokens = new ArrayList<>();
+
+        StringBuilder currentString = new StringBuilder();
+        int varId = 0;
 
         for (int i = 0; i < content.length(); i++) {
             char ch = content.charAt(i);
 
             if (ch == ' ') {
-                tokens.add("<whit>");
+                String tokenType = verifyTokenType(currentString.toString());
+
+                if (tokenType.equals("id"))
+                    tokens.add(new TokenDTO(tokenType, currentString.toString(), varId++));
+
+                else if (tokenType.equals("num"))
+                    tokens.add(new TokenDTO(tokenType, currentString.toString()));
+
+                else
+                    tokens.add(new TokenDTO(tokenType));
+
+                currentString = new StringBuilder();
+
+            } else {
+                if (ch == '\n')
+                    tokens.add(new TokenDTO("EOL"));
+
+                if (ch == '\'') {
+                    if (!currentString.toString().equals(" ")) {
+                        tokens.add(new TokenDTO("str", currentString.toString(), varId++));
+                        currentString = new StringBuilder();
+                    }
+                    tokens.add(new TokenDTO("'"));
+                }
+
+                else
+                    currentString.append(ch);
             }
 
         }
@@ -28,9 +59,28 @@ public class Main {
         return tokens;
     }
 
-    public static void printTokens(ArrayList<String> tokensList){
-        for(String tok : tokensList){
+    public static void printTokens(ArrayList<TokenDTO> tokensList){
+        for(TokenDTO tok : tokensList){
             System.out.println(tok);
         }
+    }
+
+    public static String verifyTokenType(String word){
+
+        char ch = word.charAt(0);
+
+        if (word.equals("=") || word.equals("==") || word.equals(">=") || word.equals("<=") || word.equals("*")
+                || word.equals("+") || word.equals("-") || word.equals("#") || word.equals("/") || word.equals("fun")
+                || word.equals("(") || word.equals(")") || word.equals("{") || word.equals("}") || word.equals("var")
+        )
+            return word;
+
+        if (word.equals("int") || word.equals("String") || word.equals("Double"))
+            return "typ";
+
+        if (Character.isDigit(ch))
+            return "num";
+
+        return "id";
     }
 }
---
+++ src/dto/TokenDTO.java
@@ -0,0 +1,60 @@
+package dto;
+
+public class TokenDTO {
+
+    private String token;
+    private Integer id;  // O tipo Integer permite que o ID seja nulo
+    private String value;
+
+    public TokenDTO(String token, String value, Integer id) {
+        this.token = token;
+        this.id = id;
+        this.value = value;
+    }
+
+    public TokenDTO(String token, String value) {
+        this.token = token;
+        this.value = value;
+        this.id = null;
+    }
+
+    public TokenDTO(String token) {
+        this.token = token;
+        this.value = null;
+        this.id = null;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        if (id != null && value != null)
+            return "<" + token + ", " + id + "," + value +">";
+        if (id == null && value != null)
+            return "<" + token + "," + value +">";
+        else
+            return "<" + token + ">";
+    }
+}
